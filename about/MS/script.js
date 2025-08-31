609b574641a1f538fb779b807bad22981d50f5ae
Mahdiisdumb <mahdihamade6789010@outlook.com>
2025-08-31 14:45:28 -0400

    Add files via upload

---
+++ about/MS/script.js
@@ -0,0 +1,6 @@
+document.getElementById("back").onclick = function () {
+    location.href = "../../load.html"; // Redirects to the parent directory and then to load.html
+};
+document.getElementById("backa").onclick = function () {
+    location.href = "../run.html"; // Redirects to the parent directory and then to load.html
+};
---
+++ about/script.js
@@ -0,0 +1,6 @@
+document.getElementById("back").onclick = function () {
+    location.href = "../load.html"; // Redirects to the parent directory and then to load.html
+};
+document.getElementById("MS").onclick = function () {
+    location.href = "MS/run.html"; // Redirects to the parent directory and then to load.html
+};
